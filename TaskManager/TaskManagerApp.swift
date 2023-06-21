1907a232953e906c0655668dc61557bcbe95098d
LusiMe <lusifromrussia@gmail.com>
2023-06-21 18:07:50 +0200

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Людмила Парфенова on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
