b6c83817e829af92a2c0bba5251acf8b5466cbba
LusiMe <lusifromrussia@gmail.com>
2023-07-25 15:52:36 +0200

    attempts to implement onDrop of castom obj

--- TaskManager/MatrixSort/DroppableList.swift
+++ TaskManager/MatrixSort/DroppableList.swift
@@ -11,13 +11,16 @@ enum ListId {
 
 struct DroppableList: View {
     let title: String
-    @Binding var tasks: [String]
+    @Binding var tasks: [ToDoTask]
     @Binding var listId: String
     
-    init(_ title: String, tasks: Binding<[String]>, listId:Binding<String>) {
+    let action: ((ToDoTask, Int) -> Void)?
+    
+    init(_ title: String, tasks: Binding<[ToDoTask]>, listId:Binding<String>, action: ((ToDoTask, Int) -> Void)?) {
         self.title = title
         self._tasks = tasks
         self._listId = listId
+        self.action = action
     }
     
     var body: some View {
@@ -25,15 +28,13 @@ struct DroppableList: View {
             List {
                 Text(title)
                     .font(.subheadline)
+                    .onDrop(of: [.plainText], isTargeted: nil, perform: dropOnEmptyList)
                 ForEach(tasks, id:\.self) { task in
-                    Text(task)
-                        .onDrag {
-                            listId = self.title.camelized
-                            print(listId)
-                            return NSItemProvider(object: task as NSString)
-                        }
+                    Text(task.title)
+                        .onDrag {NSItemProvider(object: task as ToDoTask)}
                 }
                 .onMove(perform: moveUser)
+                .onInsert(of: [ToDoTaskUTI], perform: dropTask)
             }
         }
     }
@@ -42,27 +43,45 @@ struct DroppableList: View {
         tasks.move(fromOffsets: source, toOffset: destination)
     }
     
-    private func dropTask(at index: Int, tasks: [ToDoTask]) {
-        
+    private func dropTask(at index: Int, _ items: [NSItemProvider]) {
+        for item in items {
+            _ = item.loadObject(ofClass: ToDoTask.self) { droppedTask, _ in
+                if let ss = droppedTask, let dropAction = action {
+                    DispatchQueue.main.async {
+                        dropAction(ss, index)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func dropOnEmptyList(items: [NSItemProvider]) -> Bool {
+        dropTask(at: tasks.endIndex, items)
+        return true
     }
 }
+
 
 struct TwoListsView: View {
     
     @State var tasks: [TaskPriority: [ToDoTask]] = [:]
     
-    @State var urgent = ["Wash the dishes", "Pay Gay's insurence", "Go to the gym"]
-    @State var notUrgent = ["Meet with friends", "Cook the dinner", "Plan vacation"]
-    @State var important = [""]
-    @State var notImportant = [""]
+    @State var urgent = [ToDoTask(id: "123", name: "Wash the dishes", priority: .urgent),
+                         ToDoTask(id: "321", name: "Pay Gay's insurence", priority: .urgent)]
+//    ["Wash the dishes", "Pay Gay's insurence", "Go to the gym"]
+    @State var notUrgent = [ToDoTask(id: "113", name: "Cook the dinner", priority: .notUrgent),
+                           ToDoTask(id: "311", name: "Meet with friends", priority: .notUrgent)]
+//    ["Meet with friends", "Cook the dinner", "Plan vacation"]
+    @State var important = [ToDoTask(id: "111", name: "nnd", priority: .important)]
+    @State var notImportant = [ToDoTask(id: "444", name: "jdkds", priority: .notImportant)]
     
     @State var fromList = ""
     
     mutating func addTasks() {
-        let task1 = ToDoTask(id: "123", note: nil, priority: .urgent)
-        let task2 = ToDoTask(id: "124", note: nil, priority: .important)
-        tasks[task2.priority] = [task2]
-        tasks[.urgent, default: []].append(task1)
+//        let task1 = ToDoTask(id: "123", note: nil, priority: .urgent)
+//        let task2 = ToDoTask(id: "124", note: nil, priority: .important)
+//        tasks[task2.priority] = [task2]
+//        tasks[.urgent, default: []].append(task1)
         print(tasks)
     }
     
@@ -79,38 +98,33 @@ struct TwoListsView: View {
             }
             
             HStack(spacing: 0) {
-                DroppableList(TaskPriority.urgent.rawValue, tasks: $urgent, listId: $fromList)
-                    .dropDestination(for: ToDoTask.self) { droppedTask, index in
-                        tasks[TaskPriority.urgent, default: []].append(contentsOf: droppedTask)
-                        guard let origin = TaskPriority(rawValue: fromList) else {return false}
-                        tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
+                DroppableList(TaskPriority.urgent.rawValue, tasks: $urgent, listId: $fromList) { droppedTask, index in
+                    tasks[TaskPriority.urgent, default: []].append(droppedTask)
+                        guard let origin = TaskPriority(rawValue: fromList) else {return }
+                        tasks[origin]?.removeAll {$0.id == droppedTask.id}
                         return true
                     }
                 
-                DroppableList("Not urgent", tasks: $notUrgent, listId: $fromList)
-                    .dropDestination(for: ToDoTask.self) { droppedTask, index in
-                        tasks[TaskPriority.notUrgent, default: []].append(contentsOf: droppedTask)
-                        guard let origin = TaskPriority(rawValue: fromList) else {return false}
-                        tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
-                        return true
-                    }
+                DroppableList("Not urgent", tasks: $notUrgent, listId: $fromList) { droppedTask, index in
+                    tasks[TaskPriority.notUrgent, default: []].append(contentsOf: droppedTask)
+                    guard let origin = TaskPriority(rawValue: fromList) else {return }
+                    tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
+                }
             }
             HStack(spacing: 0) {
-                DroppableList("Important", tasks: $important, listId: $fromList)
-                    .dropDestination(for: ToDoTask.self) { droppedTask, index in
-                        tasks[TaskPriority.important, default: []].append(contentsOf: droppedTask)
-                        guard let origin = TaskPriority(rawValue: fromList) else {return false}
-                        tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
-                        return true
-                    }
-                DroppableList("Not important", tasks: $notImportant, listId: $fromList)
-                    .dropDestination(for: ToDoTask.self) { droppedTask, index in
-                        print("task priority", TaskPriority(rawValue: fromList))
-                        tasks[TaskPriority.notImportant, default: []].append(contentsOf: droppedTask)
-                        guard let origin = TaskPriority(rawValue: fromList) else {return false}
-                        tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
-                        return true
-                    }
+                DroppableList("Important", tasks: $important, listId: $fromList) { droppedTask, index in
+                    tasks[TaskPriority.important, default: []].append(contentsOf: droppedTask)
+                    guard let origin = TaskPriority(rawValue: fromList) else {return false}
+                    tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
+                    return true
+                }
+                DroppableList("Not important", tasks: $notImportant, listId: $fromList) { droppedTask, index in
+                    print("task priority", TaskPriority(rawValue: fromList))
+                    tasks[TaskPriority.notImportant, default: []].append(contentsOf: droppedTask)
+                    guard let origin = TaskPriority(rawValue: fromList) else {return false}
+                    tasks[origin]?.removeAll {$0.id == droppedTask[0].id}
+                    return true
+                }
             }
         }
     }
--- TaskManager/MatrixSort/TaskModel.swift
+++ TaskManager/MatrixSort/TaskModel.swift
@@ -3,6 +3,8 @@ import Foundation
 import SwiftUI
 import UniformTypeIdentifiers
 
+let ToDoTaskUTI: String = UTType.data.identifier
+
 enum TaskPriority: String, Codable, CaseIterable {
   case notImportant
   case notUrgent
@@ -19,25 +21,63 @@ enum TaskPriority: String, Codable, CaseIterable {
   }
 }
 
-struct ToDoTask: Codable, Identifiable, Transferable {
+final class ToDoTask: NSObject, Codable, NSItemProviderWriting, NSItemProviderReading, _ObjectiveCBridgeable {
+    func _bridgeToObjectiveC() -> NSObject {
+        <#code#>
+    }
+    
+    static func _forceBridgeFromObjectiveC(_ source: NSObject, result: inout ToDoTask?) {
+        result = ToDoTask(id: source.id, name: source.name, priority: source.priority)
+    }
+    
+    static func _conditionallyBridgeFromObjectiveC(_ source: NSObject, result: inout ToDoTask?) -> Bool {
+        <#code#>
+    }
+    
+    static func _unconditionallyBridgeFromObjectiveC(_ source: NSObject?) -> Self {
+        <#code#>
+    }
+    
+    typealias _ObjectiveCType = AnyClass
+    
+    
+    static var readableTypeIdentifiersForItemProvider: [String] = [ToDoTaskUTI]
+
+    static var writableTypeIdentifiersForItemProvider: [String] = [ToDoTaskUTI]
+    
+    func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping @Sendable (Data?, Error?) -> Void) -> Progress? {
+        let progress = Progress(totalUnitCount: 100)
+              guard typeIdentifier == ToDoTaskUTI else {
+                  return progress
+              }
+              do {
+                  let jsonEncoder = JSONEncoder()
+                  let data = try jsonEncoder.encode(self)
+                  completionHandler(data, nil)
+              }
+              catch { completionHandler(nil, error) }
+              return progress
+          }
+    
+    
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Self {
+        let jsonDecoder = JSONDecoder()
+    let item = try! jsonDecoder.decode(Self.self, from: data)
+               return item
+    }
+
+    
   let id: String
   let title: String
-  let note: String?
+  let name: String
   let priority: TaskPriority
     
     
-  init(id: String, note: String?, priority: TaskPriority) {
+  init(id: String, name: String, priority: TaskPriority) {
     self.id = id
     self.title = priority.rawValue
-    self.note = note
+    self.name = name
     self.priority = priority
   }
-    
-    static var transferRepresentation: some TransferRepresentation {
-        CodableRepresentation(contentType: .toDoTask)
-    }
 }
 
-extension UTType{
-    static let toDoTask = UTType(exportedAs: "test.first.luda.TaskManager")
-}
